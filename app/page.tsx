901d7f44f03db47dfc8d172ad7093a6ab6f48b52
Park JunYong <dyden119@gmail.com>
2024-05-29 00:50:37 +0900

    design: 글로벌css설정, 헤더 퍼블리싱

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import Header from '../components/header';
+import styles from './page.module.css';
 
 export default function Main() {
   return (
-    <div>
+    <div className={styles.container}>
       <Header />
       <h1>메인페이지</h1>
     </div>
--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,10 +1,24 @@
 import Link from 'next/link';
+import styles from './index.module.css';
+import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header>
-      헤더입니다.
-      <Link href="/sign-in">로그인</Link>
+    <header className={styles.header}>
+      <div>
+        <Link href="/">
+          <Image
+            src="/icons/logo.svg"
+            width={133}
+            height={24}
+            alt="로고"
+            className={styles.logo}
+          />
+        </Link>
+        <Link href="/sign-in">
+          <div className={styles.button}>로그인</div>
+        </Link>
+      </div>
     </header>
   );
 }
