d8ad0e2e31e3eba78c3ea5d81f3b9a16f8afa3c1
Park JunYong <dyden119@gmail.com>
2024-06-01 20:43:05 +0900

    feat: 로그인/회원가입 페이지 비밀번호 input 텍스트 표시/비표시 토글

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,10 +1,17 @@
 'use client';
 
+import { useState } from 'react';
 import Image from 'next/image';
 import style from './page.module.css';
 import Link from 'next/link';
 
 export default function SignIn() {
+  const [isVisivle, setIsVisivle] = useState(false);
+
+  const handleIsVisible = () => {
+    setIsVisivle((current) => !current);
+  };
+
   const WIP = () => {
     alert('개발중입니다.');
   };
@@ -24,7 +31,14 @@ export default function SignIn() {
           <h2>이메일</h2>
           <input type='text' />
           <h2>비밀번호</h2>
-          <input type='password' />
+          <input type={isVisivle ? 'text' : 'password'} />
+          <Image
+            src={isVisivle ? '/icons/eye-on.svg' : '/icons/eye-off.svg'}
+            width={16}
+            height={16}
+            alt='비밀번호 노출 아이콘'
+            onClick={handleIsVisible}
+          />
         </div>
         <div className={style.buttonBox}>
           <div className={style.loginButton} onClick={WIP}>
--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,10 +1,17 @@
 'use client';
 
+import { useState } from 'react';
 import Image from 'next/image';
 import style from './page.module.css';
 import Link from 'next/link';
 
 export default function SignUp() {
+  const [isVisivle, setIsVisivle] = useState([false, false]);
+
+  const handleIsVisible = (index: number) => {
+    setIsVisivle((current) => current.map((visibility, idx) => (idx === index ? !visibility : visibility)));
+  };
+
   const WIP = () => {
     alert('개발중입니다.');
   };
@@ -24,9 +31,23 @@ export default function SignUp() {
           <h2>이메일</h2>
           <input type='text' />
           <h2>비밀번호</h2>
-          <input type='password' />
+          <input type={isVisivle[0] ? 'text' : 'password'} />
+          <Image
+            src={isVisivle[0] ? '/icons/eye-on.svg' : '/icons/eye-off.svg'}
+            width={16}
+            height={16}
+            alt='비밀번호 노출 아이콘'
+            onClick={() => handleIsVisible(0)}
+          />
           <h2>비밀번호 확인</h2>
-          <input type='password' />
+          <input type={isVisivle[1] ? 'text' : 'password'} />
+          <Image
+            src={isVisivle[1] ? '/icons/eye-on.svg' : '/icons/eye-off.svg'}
+            width={16}
+            height={16}
+            alt='비밀번호 노출 아이콘'
+            onClick={() => handleIsVisible(1)}
+          />
         </div>
         <div className={style.buttonBox}>
           <div className={style.loginButton} onClick={WIP}>
