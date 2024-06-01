b13683cbb82bbcf75975c664f507000a5ed0c31b
Park JunYong <dyden119@gmail.com>
2024-06-02 01:57:43 +0900

    feat: 로그인페이지 상황별 경고메시지 구현

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -7,9 +7,30 @@ import Link from 'next/link';
 
 export default function SignIn() {
   const [isVisivle, setIsVisivle] = useState(false);
+  const [isInvalidateEmail, setIsInvalidateEmail] = useState(false);
+  const [isInvalidatePassword, setIsInvalidatePassword] = useState(false);
+  const [typeOfEmailError, setTypeOfEmailError] = useState('');
 
   const handleIsVisible = () => {
     setIsVisivle((current) => !current);
+  };
+
+  const handleInputWarning = (e: React.FocusEvent<HTMLInputElement>) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const value = e.target.value;
+    if (e.target.name === 'email') {
+      if (value === '') {
+        setIsInvalidateEmail((current) => !current);
+        setTypeOfEmailError('이메일을 입력해주세요');
+      } else if (!emailRegex.test(value)) {
+        setIsInvalidateEmail((current) => !current);
+        setTypeOfEmailError('올바른 이메일 주소가 아닙니다.');
+      }
+    } else if (e.target.name === 'password') {
+      if (value === '') {
+        setIsInvalidatePassword((current) => !current);
+      }
+    }
   };
 
   const WIP = () => {
@@ -28,17 +49,37 @@ export default function SignIn() {
           </span>
         </div>
         <div className={style.inputBox}>
-          <h2>이메일</h2>
-          <input type='text' />
-          <h2>비밀번호</h2>
-          <input type={isVisivle ? 'text' : 'password'} />
-          <Image
-            src={isVisivle ? '/icons/eye-on.svg' : '/icons/eye-off.svg'}
-            width={16}
-            height={16}
-            alt='비밀번호 노출 아이콘'
-            onClick={handleIsVisible}
-          />
+          <label htmlFor='email'>
+            <h2>이메일</h2>
+            <input
+              id='email'
+              type='text'
+              name='email'
+              onBlur={(e) => handleInputWarning(e)}
+              onFocus={() => setIsInvalidateEmail(false)}
+              className={isInvalidateEmail ? style.invalidate : ''}
+            />
+          </label>
+          <h6 className={isInvalidateEmail ? '' : style.disabled}>{isInvalidateEmail && typeOfEmailError}</h6>
+          <label htmlFor='password'>
+            <h2>비밀번호</h2>
+            <input
+              id='password'
+              type={isVisivle ? 'text' : 'password'}
+              name='password'
+              onBlur={(e) => handleInputWarning(e)}
+              onFocus={() => setIsInvalidatePassword(false)}
+              className={isInvalidatePassword ? style.invalidate : ''}
+            />
+            <Image
+              src={isVisivle ? '/icons/eye-on.svg' : '/icons/eye-off.svg'}
+              width={16}
+              height={16}
+              alt='비밀번호 노출 아이콘'
+              onClick={handleIsVisible}
+            />
+          </label>
+          <h6>{isInvalidatePassword ? '비밀번호를 입력해주세요' : ''}</h6>
         </div>
         <div className={style.buttonBox}>
           <div className={style.loginButton} onClick={WIP}>
