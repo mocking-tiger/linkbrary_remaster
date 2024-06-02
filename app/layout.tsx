e0ed17640bc669a1994562d91cc677415ec7ed22
Park JunYong <dyden119@gmail.com>
2024-06-02 20:15:06 +0900

    chore: 오픈그래프 이미지 테스트

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,13 @@
-import { Metadata } from 'next';
 import '../styles/globals.css';
 
-export const metadata: Metadata = {
+export const metadata = {
   title: {
     template: 'Linkbrary | %s',
     default: 'Linkbrary',
   },
   description: '세상의 모든 정보를 쉽게 저장하고 관리해 보세요.',
-  openGraph: {
-    images: '/images/meta-thumbnail.png',
-  },
+  keyword: ['link', 'linkbrary', '즐겨찾기', '바로가기', '링크', '링크브러리', '공유', '편리', '정보'],
+  ogImage: '/images/meta-thumbnail.png',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
