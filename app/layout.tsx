fffd059bf6675391f21108521ff5d67c8d563a50
Park JunYong <dyden119@gmail.com>
2024-06-02 20:39:51 +0900

    chore: 오픈그래프 이미지 최종 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,34 @@
+import { Metadata } from 'next';
 import '../styles/globals.css';
 
-export const metadata = {
+export const metadata: Metadata = {
+  metadataBase: new URL('https://linkbrary-remaster.vercel.app/'),
   title: {
     template: 'Linkbrary | %s',
     default: 'Linkbrary',
   },
   description: '세상의 모든 정보를 쉽게 저장하고 관리해 보세요.',
+  openGraph: {
+    images: [
+      {
+        url: '/images/meta-thumbnail.png',
+        alt: '링크브러리 오픈그래프 이미지',
+      },
+    ],
+  },
+  twitter: {
+    images: [
+      {
+        url: '/images/meta-thumbnail.png',
+        alt: '링크브러리 트위터 이미지',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='ko'>
-      <head>
-        <meta property='og:image' content='/images/meta-thumbnail.png' />
-      </head>
       <body>{children}</body>
     </html>
   );
