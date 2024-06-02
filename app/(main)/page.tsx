fc82005321961b6e9882d24ab104e0de479ab022
Park JunYong <dyden119@gmail.com>
2024-06-02 16:30:07 +0900

    docs: 메인페이지 라우팅

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,8 +1,8 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from './page.module.css';
-import Header from '../components/header';
-import Footer from '../components/footer';
+import Header from '../../components/header';
+import Footer from '../../components/footer';
 
 export default function Main() {
   return (
