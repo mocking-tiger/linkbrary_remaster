9eaa03fe46150d6a211a278b8c5bf5d456553e73
Park JunYong <dyden119@gmail.com>
2024-06-05 12:23:02 +0900

    chore: 스토어 기본값 작성

--- zustand/userStore.ts
+++ zustand/userStore.ts
@@ -2,14 +2,31 @@ import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 import { UserInfoType } from '../components/types/types';
 
+interface UserState {
+  userInfo: UserInfoType;
+  setUserInfo: (userInfo: UserInfoType) => void;
+  clearUserInfo: () => void;
+}
+
+const initialState = {
+  auth_id: '',
+  created_at: '',
+  email: '',
+  id: 0,
+  image_source: '',
+  name: '',
+};
+
 export const useUserStore = create(
-  persist(
+  persist<UserState>(
     (set) => ({
-      userInfo: {},
+      userInfo: {
+        ...initialState,
+      },
       setUserInfo: (userInfo: UserInfoType) => {
         set(() => ({ userInfo }));
       },
-      clearUserInfo: () => set({ userInfo: {} }),
+      clearUserInfo: () => set({ userInfo: { ...initialState } }),
     }),
     { name: 'userInfo' },
   ),
